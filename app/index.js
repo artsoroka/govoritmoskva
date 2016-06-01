68c11c3d2bfb83549a314d667f923ae81f9bc4f3
artsoroka <githubrepository@yandex.ru>
2016-06-01 16:40:18 +0000

    log all requests

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 var express    = require('express'); 
 var bodyParser = require('body-parser'); 
 var app        = express();
+var logger     = require('./lib/logger'); 
 var config     = require('../config'); 
 
 app.use(bodyParser.json()); 
 
 app.all('*', function(req, res){
-    res.json(config); 
+    logger.info('TG webhook recieved', req.body); 
+    res.send('ok'); 
 }); 
 
 module.exports = app; 
