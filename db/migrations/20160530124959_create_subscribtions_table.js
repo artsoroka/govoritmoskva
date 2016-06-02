7f18b58dcdc60e3bdf6d150e03f37f4c3a975682
Artem Soroka <9325033@gmail.com>
2016-06-02 21:30:46 +0000

    add subscribtions table

---
+++ db/migrations/20160530124959_create_subscribtions_table.js
@@ -0,0 +1,20 @@
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.createTable('subscribtions', function(table){
+            table.charset('utf8'); 
+            table.collate('utf8_unicode_ci'); 
+
+            table.increments(); 
+            table.integer('chat_id').unsigned();
+            table.string('action', 12); // subscribe | unsibscribe 
+            table.string('first_name', 100);
+            table.string('last_name', 100); 
+            table.integer('ts').unsigned();
+            table.timestamp("created_at").defaultTo(knex.raw('now()')); 
+        })
+    ]);
+};
+
+exports.down = function(knex, Promise) {
+    knex.schema.dropTableIfExists('users'); 
+};
